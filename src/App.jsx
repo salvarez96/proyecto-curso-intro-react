88f04dc88c863721b4af615f215e8303152c2df0
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-01-07 23:54:21 -0500

    Creando la base de la app y añadiendo styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import React from 'react';
+import TodoCounter from './components/TodoCounter';
+import TodoSearch from './components/TodoSearch';
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
+import CreateTodoButton from './components/CreateTodoButton';
 
 function App() {
   return (
     <>
       <h1>Hay alguien aquí con vida :v</h1>
-      {/* <TodoCounter />
+      <TodoCounter />
       <TodoSearch />
       <TodoList>
         <TodoItem />
       </TodoList>
-      <CreateTodoButton /> */}
+      <CreateTodoButton />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalStyles } from "./globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <App />
+  <>
+    <GlobalStyles />
+    <App />
+  </>
 );
