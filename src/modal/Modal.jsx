e93cd2f1cd00ccf1e0bb5d1f740b6f6862d142df
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-01-14 22:30:00 -0500

    Modificado el modal para implementar un formulario
    Movido uuid de las dev dependencies

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { TodoGlobalContext } from '../context/Provider';
-import { InputText } from '../components/TodoSearch';
 import { v4 as uuidv4 } from 'uuid';
-
 
 const Section = styled.section`
   width: 100%;
@@ -13,7 +11,7 @@ const Section = styled.section`
   top: 0;
 `;
 
-const Div = styled.div`
+const Form = styled.form`
   width: 80%;
   max-width: 400px;
   min-height: 200px;
@@ -25,9 +23,9 @@ const Div = styled.div`
   padding: 20px 15px;
   text-align: center;
 
-  & input[type='text'] {
-    margin: 20px 0;
-    border-radius: 10px;
+  & label {
+    font-size: calc(18rem / 16);
+    font-weight: bold;
   }
 `;
 
@@ -57,9 +55,38 @@ const Button = styled.button`
   }
 `;
 
+const TextArea = styled.textarea`
+  width: 100%;
+  min-height: 55px;
+  margin: 15px 0;
+  border: 2px double var(--pastel-blue);
+  border-radius: 10px;
+  padding: 10px;
+  background-color: var(--very-dark-purple);
+  font-family: inherit;
+  color: white;
+  font-size: calc(17rem / 16);
+
+  &:focus-visible {
+    outline: none;
+    border-color: var(--orange);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(26, 27, 75, 0.5);
+    border-radius: 5px;
+  }
+  
+  &::-webkit-scrollbar {
+    background-color: rgba(255, 255, 255, 0.2);
+    width: 12px;
+    border-bottom-right-radius: 8px;
+  }
+`;
+
 export default function Modal() {
 
-  const {modalOpen, todos, saveTodos} = React.useContext(TodoGlobalContext);
+  const {modalOpen, setModalOpen, todos, saveTodos} = React.useContext(TodoGlobalContext);
 
   const [inputValue, setInputValue] = React.useState('');
 
@@ -67,9 +94,11 @@ export default function Modal() {
     setInputValue(e.target.value);
   }
 
-  const getNewTodo = () => {
+  const getNewTodo = e => {
+    e.preventDefault();
     if(inputValue.length > 0) {
       const newTodo = {
+        // con uuidv4 puedo crear ids únicos
         id: uuidv4(),
         task: inputValue.trim(),
         complete: false
@@ -77,29 +106,30 @@ export default function Modal() {
       saveTodos([newTodo, ...todos]);
     }
     setInputValue('');
+    setModalOpen(!modalOpen);
   }
 
   return (
     <>
       {modalOpen &&
         <Section>
-          <Div>
-            <h3>Agrega una tarea nueva</h3>
-            <InputText
+          <Form onSubmit={getNewTodo}>
+            <label htmlFor='text-area'>Agrega una tarea nueva</label>
+            <TextArea
               type='text'
-              placeholder='Dominar el mundo. . .'
+              id='text-area'
+              placeholder='Tratar de conquistar el mundo. . .'
               onChange={getInputValue}
               value={inputValue}
             />
             <Button 
-              type='button' 
+              type='submit' 
               className={`add-button${inputValue.length < 1 ? ' disabled' : ' enabled'}`}
-              onClick={getNewTodo}
               disabled={inputValue.length < 1}
             >
               Agregar
             </Button>
-          </Div>
+          </Form>
         </Section>
       }
     </>
